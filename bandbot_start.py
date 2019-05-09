6511b2a943413d6f92728512630cb7ef743702d9
kohs100 <kohs102@gmail.com>
2019-05-10 02:12:34 +0900

    Update bandbot_start.py

--- bandbot_start.py
+++ bandbot_start.py
@@ -19,6 +19,8 @@ def bothelp(msgWrite, isWrong):
 	else:
 		msgWrite.send_keys("[" + param.NAME + "] " + param.version)
 		msgWrite.send_keys(Keys.SHIFT, Keys.ENTER)
+		msgWrite.send_keys("https://github.com/kohs100/bandbot2")
+		msgWrite.send_keys(Keys.SHIFT, Keys.ENTER)
 		msgWrite.send_keys("지원되는 명령어 : ")
 		msgWrite.send_keys(Keys.SHIFT, Keys.ENTER)
 		msgWrite.send_keys("!" + param.NAME + " 밀리이벤, 밀리이벤컷, 주사위 , 시어터, 라이브런")
