8da013e9e41b78d1f8c30343b5f254761f548fa9
kohs100 <kohs102@gmail.com>
2019-09-09 12:48:05 +0900

    Update param.py

--- param.py
+++ param.py
@@ -2,8 +2,8 @@ NAME = "촉수봇"
 version = "Beta 0.9.98"
 
 #BandchatURL = "https://band.us/band/55800178/chat/C6HumD"
-#TestchatURL = "https://band.us/band/55800178/chat/CNhhG4"
-chatURL = "https://band.us/band/55800178/chat/C6HumD"
+chatURL = "https://band.us/band/55800178/chat/CNhhG4"
+#chatURL = "https://band.us/band/55800178/chat/C6HumD"
 
 ChromeDevPort = "9222"
 WebdriverPort = "33333"
