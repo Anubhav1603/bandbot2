4c8c1e611e9265e2ee1aebcf76fa14c5d5dddbd9
kohs100 <kohs102@gmail.com>
2020-02-27 12:31:32 +0900

    bandChat class

--- param.py
+++ param.py
@@ -1,6 +1,5 @@
 NAME = "밴드봇"
 version = "2.0.1"
 
-#bandchatURL = "https://band.us/band/55800178/chat/C6HumD"
 testchatURL = "https://band.us/band/77955502/chat/CQvbxV"
 chatURL = "https://band.us/band/55800178/chat/C6HumD"
--- start.py
+++ start.py
@@ -10,77 +10,89 @@ import sys
 
 from time import sleep, strftime, time
 
-def getDriver():
-	chromeOptions = {"debuggerAddress":"127.0.0.1:9222"}
-	capabilities = {"chromeOptions":chromeOptions}
-	print("Driver initializing...")
-	driver = webdriver.Remote("http://127.0.0.1:33333", capabilities)
-	return driver
+class bandChat():
+	def get_driver(self):
+		chromeOptions = {"debuggerAddress":"127.0.0.1:9222"}
+		capabilities = {"chromeOptions":chromeOptions}
+		print("Driver initializing...")
+		self.driver = webdriver.Remote("http://127.0.0.1:33333", capabilities)
+		
+	def get_msgWrite(self):
+		startsec = time()
+		while(True):
+			try:
+				self.msgWrite = self.driver.find_element_by_class_name("commentWrite")
+			except:
+				if(time() > startsec+20):
+					now = datetime.datetime.now()
+					print("CHAT LOAD ERROR at " + now.isoformat())
+					raise chatError
+					exit()
+				continue
+			break
+		sleep(1)
+		print("[" + param.NAME + "] " + param.version + " boot success")
 
-def initLogin(isTest = False):
-	driver = getDriver()
-	print("Driver initialized.")
+	def __init__(self, isTest):
+		self.chatURL = param.testchatURL if isTest else param.chatURL
+		self.isTest = isTest
 
-	driver.get(param.testchatURL if isTest else param.chatURL)
-	print("Get login page completed.")
-	driver.implicitly_wait(3)
+		self.get_driver()
+		print("Driver initialized.")
 
-	driver.find_element_by_css_selector(".uBtn.-icoType.-phone").click()
-	print("Get PhonenumberPage completed.")
-	driver.implicitly_wait(3)
+		self.driver.get(param.testchatURL if isTest else param.chatURL)
+		print("Get login page completed.")
+		self.driver.implicitly_wait(3)
 
-	Phonenumber = input("전화번호 입력 :")
-	driver.find_element_by_id("input_local_phone_number").send_keys(Phonenumber)
-	driver.find_element_by_css_selector(".uBtn.-tcType.-confirm").click()
-	print("Get PasswordPage completed.")
-	driver.implicitly_wait(3)
+		self.driver.find_element_by_css_selector(".uBtn.-icoType.-phone").click()
+		print("Get PhonenumberPage completed.")
+		self.driver.implicitly_wait(3)
 
-	Password = input("비밀번호 입력 :")
-	driver.find_element_by_id("pw").send_keys(Password)
-	driver.find_element_by_css_selector(".uBtn.-tcType.-confirm").click()
-	print("Get SMSPage completed.")
-	driver.implicitly_wait(8)
-	try:
-		print(driver.find_element_by_id("hintNumberDiv").text)
-		sleep(20)
-	except NoSuchElementException:
-		pw_band=input("인증번호: ")
-		driver.find_element_by_id("code").send_keys(str(pw_band))
-		driver.find_element_by_css_selector("button.uBtn.-tcType.-confirm").click();
-		print("Driver get completed.")
-	
+		Phonenumber = input("전화번호 입력 :")
+		self.driver.find_element_by_id("input_local_phone_number").send_keys(Phonenumber)
+		self.driver.find_element_by_css_selector(".uBtn.-tcType.-confirm").click()
+		print("Get PasswordPage completed.")
+		self.driver.implicitly_wait(3)
 
-	msgWrite = loadingWait(driver)
-	return driver, msgWrite
-
-def loadingWait(driver):
-	startsec = time()
-	while(True):
+		Password = input("비밀번호 입력 :")
+		self.driver.find_element_by_id("pw").send_keys(Password)
+		self.driver.find_element_by_css_selector(".uBtn.-tcType.-confirm").click()
+		print("Get SMSPage completed.")
+		self.driver.implicitly_wait(8)
 		try:
-			msgWrite = driver.find_element_by_class_name("commentWrite")
-		except:
-			if(time() > startsec+20):
-				now = datetime.datetime.now()
-				print("CHAT LOAD ERROR at " + now.isoformat())
-				raise ChatError
-				exit()
-			continue
-		break
-	sleep(1)
-	print("[" + param.NAME + "] " + param.version + " boot success")
-	return msgWrite
+			print(self.driver.find_element_by_id("hintNumberDiv").text)
+			sleep(20)
+		except NoSuchElementException:
+			pw_band = input("인증번호: ")
+			self.driver.find_element_by_id("code").send_keys(str(pw_band))
+			self.driver.find_element_by_css_selector("button.uBtn.-tcType.-confirm").click();
+			print("Driver get completed.")
+		self.get_msgWrite(driver)
 
-def loginRefresh(isTest = False):
-	driver = getDriver()
-	print("Driver initialized.")
-	driver.get(param.testchatURL if isTest else param.chatURL)
-	print("Driver get completed.")
-	msgWrite = loadingWait(driver)
-	driver.implicitly_wait(30)
-	if isTest:
-		msgWrite.send_keys("[" + param.NAME + "] 새로고침 완료")
-		msgWrite.send_keys(Keys.ENTER)
-	return driver, msgWrite
+	def loginRefresh(self):
+		self.driver = get_driver()
+		print("Driver initialized.")
+		self.driver.get(self.chatURL)
+		print("Driver get completed.")
+		self.get_msgWrite()
+		driver.implicitly_wait(30)
+
+		if self.isTest:
+			self.msgWrite.send_keys("[" + param.NAME + "] 새로고침 완료")
+			self.msgWrite.send_keys(Keys.ENTER)
+
+	def chatPrint(self, str_i):
+		lines = str_i.split("\n")
+		for chat in lines:
+			self.msgWrite.send_keys(chat)
+			self.msgWrite.send_keys(Keys.SHIFT, Keys.ENTER)
+		self.msgWrite.send_keys(Keys.ENTER)
+
+	def HTMLget(self):
+		soup = BeautifulSoup(self.driver.page_source, 'html.parser')
+		chatlist = soup.find_all("span", class_="txt")
+		userlist = soup.find_all("button", class_="author")
+		return len(chatlist), chatlist, userlist
 
 def cntQst():
 	resp = input("Continue? (Y)")
@@ -115,12 +127,6 @@ def bothelp(isWrong, commands):
 
 		return responseChat
 
-def HTMLget(driver):
-	soup = BeautifulSoup(driver.page_source, 'html.parser')
-	chatlist = soup.find_all("span", class_="txt")
-	userlist = soup.find_all("button", class_="author")
-	return len(chatlist), chatlist, userlist
-		
 def CommandSel(params, usr_i, commands, mods):
 	if len(params) == 1:
 		return bothelp(False, commands)
@@ -133,37 +139,20 @@ def CommandSel(params, usr_i, commands, mods):
 	if not isCommand:
 		return bothelp(True, commands)
 
-def newChat(usr_i, str_i, commands, mods):
-	def chatPrint(str_i, msgWrite):
-		chats = str_i.split("\n")
-		for chat in chats:
-			msgWrite.send_keys(chat)
-			msgWrite.send_keys(Keys.SHIFT, Keys.ENTER)
-		msgWrite.send_keys(Keys.ENTER)
-	def sendAlarm(usr_i, str_i):
-		alarm_keywords = ["촉수","ㅎㅅㅋ","히사코"]
-		for keyword in alarm_keywords:
-			if keyword in str_i:
-				bot = teletoken.getBot()
-				msg = strftime("%H:%M ") + usr_i + " is calling you.\n" + str_i
-				teletoken.sendChat(bot, msg)
+def sendAlarm(usr_i, str_i):
+	alarm_keywords = ["촉수","ㅎㅅㅋ","히사코"]
+	for keyword in alarm_keywords:
+		if keyword in str_i:
+			bot = teletoken.getBot()
+			msg = strftime("%H:%M ") + usr_i + " is calling you.\n" + str_i
+			teletoken.sendChat(bot, msg)
 
-		alarm_users = ["레몬스타"]
-		for keyuser in alarm_users:
-			if keyuser == usr_i:
-				bot = teletoken.getBot()
-				msg = "senpai alert" + strftime("%H:%M ") + str_i
-				teletoken.sendChat(bot, msg)
-
-
-	if str_i[:2] == "!봇":
-		params = str_i.split(" ")
-		if params[0] == "!봇":
-			prefixChat = "[" + param.NAME + "] " + usr_i + "\n"
-			responseChat = CommandSel(params, usr_i, commands, mods)
-			chatPrint(prefixChat + responseChat, msgWrite)
-
-	sendAlarm(usr_i, str_i)
+	alarm_users = ["레몬스타"]
+	for keyuser in alarm_users:
+		if keyuser == usr_i:
+			bot = teletoken.getBot()
+			msg = "senpai alert" + strftime("%H:%M ") + str_i
+			teletoken.sendChat(bot, msg)
 
 if not __name__ == "__main__":
 	pass
@@ -175,12 +164,11 @@ elif len(sys.argv) == 1:
 	print("Auto refreshing and driver re-grab enabled.")
 	cntQst()
 	
+	chatRoom = bandChat(False)
 
 	timeFlag = int(strftime("%M")) < 30
 
-	driver, msgWrite = initLogin()
-
-	len_chat, i_chat, i_user = HTMLget(driver)
+	len_chat, i_chat, i_user = chatRoom.HTMLget()
 	recent_chat = len(i_chat)
 
 	commands, mods = Modules()
@@ -188,16 +176,22 @@ elif len(sys.argv) == 1:
 	while(True):
 		if (int(strftime("%M")) < 30 and timeFlag) or (int(strftime("%M")) >= 30 and not timeFlag):
 			timeFlag = not timeFlag
-			driver, msgWrite = loginRefresh(True)
+			chatRoom.loginRefresh()
 
-		len_chat, i_chat, i_user = HTMLget(driver)
+		len_chat, i_chat, i_user = chatRoom.HTMLget()
 
 		for i in range(recent_chat-len_chat,0):
 			str_i = i_chat[i].text
 			usr_i = i_user[i].text
 			print(usr_i + ":" + str_i)
 
-			newChat(usr_i, str_i, commands, mods)
+			if str_i[:2] == "!봇":
+				params = str_i.split(" ")
+				if params[0] == "!봇":
+					prefixChat = "[" + param.NAME + "] " + usr_i + "\n"
+					responseChat = CommandSel(params, usr_i, commands, mods)
+					chatRoom.chatPrint(prefixChat + responseChat)
+			sendAlarm(usr_i, str_i)
 
 		recent_chat = len_chat
 
@@ -211,21 +205,28 @@ elif sys.argv[1] == "--test":
 	print("Long-term operation cannot be guaranteed. Use at your own risk.")
 	cntQst()
 
-	driver, msgWrite = initLogin(True)
+	chatRoom = bandChat(True)
 
-	len_chat, i_chat, i_user = HTMLget(driver)
+	len_chat, i_chat, i_user = chatRoom.HTMLget()
 	recent_chat = len(i_chat)
 
 	commands, mods = Modules()
+
 	while(True):
-		len_chat, i_chat, i_user = HTMLget(driver)
+		len_chat, i_chat, i_user = chatRoom.HTMLget()
 
 		for i in range(recent_chat-len_chat,0):
 			str_i = i_chat[i].text
 			usr_i = i_user[i].text
 			print(usr_i + ":" + str_i)
-		
-			newChat(usr_i, str_i)
+
+			if str_i[:2] == "!봇":
+				params = str_i.split(" ")
+				if params[0] == "!봇":
+					prefixChat = "[" + param.NAME + "] " + usr_i + "\n"
+					responseChat = CommandSel(params, usr_i, commands, mods)
+					chatRoom.chatPrint(prefixChat + responseChat)
+			sendAlarm(usr_i, str_i)
 
 		recent_chat = len_chat
 
@@ -233,7 +234,7 @@ elif sys.argv[1] == "--test":
 
 elif sys.argv[1] == "--simple-test":
 	print("***BANDBOT STARTED IN SIMPLE TEST MODE***")
-	print("this mode tests thirdparty extensions only")
+	print("this mode tests 3rd-party extensions only")
 	print("test username is \"QwErTyTeSt\".")
 	print("type \"!exit\" to exit\n")
 
