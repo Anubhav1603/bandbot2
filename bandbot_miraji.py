52b33902d9f441975cf478a8b3346ca0c45ed9c5
kohs100 <kohs102@gmail.com>
2020-07-22 19:17:19 +0900

    Update bandbot_miraji.py

--- bandbot_miraji.py
+++ bandbot_miraji.py
@@ -15,6 +15,13 @@ def Com(params, usr_i):
         f.close()
 
     if paramNum == 3:
+        if params[2] == "갱신":
+            f = open("bandbot_miraji_dict.csv", encoding = "utf-8")
+            rdr = csv.reader(f)
+            CSV_CACHE = list(rdr)
+            f.close()
+            return "miraji.py: 미라지 갱신완료"
+            
         for elem in CSV_CACHE:
             if params[2] == elem[0]:
                 return "REQUEST_IMAGE_" + elem[1]
