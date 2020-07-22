9b9582c4750a2b7e5afcb56fcbebbb91348c7312
kohs100 <kohs102@gmail.com>
2020-07-22 19:15:41 +0900

    timeout

--- bandbot_calc.py
+++ bandbot_calc.py
@@ -1,28 +1,48 @@
-import pwnlib.util.safeeval
+import pwnlib.util.safeeval, time, multiprocessing
+from multiprocessing import Process, Queue
 
 command = ["연산"]
 
-class avg:
-    def __add__(self, lst_input):
-        try:
-            return sum(lst_input) / len(lst_input)
-        except:
-            raise ValueError
+def SaveEvaluation(sick, q): 
+    class avg:
+        def __add__(self, lst_input):
+            try:
+                return sum(lst_input) / len(lst_input)
+            except:
+                raise ValueError
 
+    try:
+        result = pwnlib.util.safeeval.values(sick, {'avg' : avg()})
+        result = str(result)
+
+    except Exception as e:
+        print(e)
+        return
+    else:
+        q.put(result)
+        return
 
 def Com(params, usr_i):
     paramNum = len(params)
 
     if paramNum > 2:
-        print(" ".join(params[2:]))
-        try:
-            sick = " ".join(params[2:])
-            return str(pwnlib.util.safeeval.values(sick, {'avg' : avg()}))
+        sick = " ".join(params[2:])
+        print(sick)
 
-        except Exception as e:
-            print(e)
-            return "calc.py: 잘못된 식"
-            
+        q = Queue()
+        p = Process(target=SaveEvaluation, args = (sick, q))
+        p.start()
+
+        p.join(10)
+
+        if p.is_alive():
+            p.terminate()
+            p.join()
+
+        if q.empty():
+            return "calc.py: 너무 오래걸립니다."
+        else:
+            return q.get()
     else:
         return "calc.py: 사용법\n"\
                "!봇 연산 [계산식]"
