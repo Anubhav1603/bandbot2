87eff3bb18e4ff041be57176b7743f3d0810b0ce
kohs100 <kohs102@gmail.com>
2020-08-07 16:45:28 +0900

    Update pstcalc.py

--- module_pstcalc/pstcalc.py
+++ module_pstcalc/pstcalc.py
@@ -18,47 +18,53 @@ def LevelToStamina(lv):
 def Com(params, usr_i):
 	paramnum = len(params)
 	guide = "pstcalc.py: 사용법\n\
-!봇 계산 [시어터|투어] [영업런|라이브런] [레벨] [목표점수]"
+!봇 계산 [시어터|투어|튠] [영업런|라이브런] [레벨] [목표점수]"
 
 	if paramnum == 6:
-		if params[2] == "시어터":	#EX) !봇 계산 시어터 영업런 160 300000
-			try:
-				workdic = {"영업런":True, "라이브런":False}
-				isWork = workdic[params[3]]
-				stamina = LevelToStamina(int(params[4]))
-				score = int(params[5])
+		try:
+			workdic = {"영업런":True, "라이브런":False}
+			isWork = workdic[params[3]]
+			stamina = LevelToStamina(int(params[4]))
+			score = int(params[5])
+			
+			if params[2] == "시어터":	#EX) !봇 계산 시어터 영업런 160 300000
 				return calcTheater(stamina, score, isWork)
-			except:
-				return guide
 
-		elif params[2] == "투어":	#EX) !봇 계산 투어 영업런 160 300000
-			try:
-				workdic = {"영업런":True, "라이브런":False}
-				isWork = workdic[params[3]]
-				return calcTour(int(params[4]), int(params[5]), isWork)
-			except:
-				return guide
-		else:
+			elif params[2] == "투어":	#EX) !봇 계산 투어 영업런 160 300000
+				return calcTour(stamina, score, isWork)
+
+			elif params[2] == "튠":
+				return calcTune(stamina, score, isWork)
+		except:
 			return guide
 	else:
 		return guide
 
-def calcTheater(Stamina, Score, isWork):
+def calcTheater(genki, score, isWork):
 	if isWork:
-		WorkResult = (Score/((170.0/60.0*0.7)+(170.0/60.0*0.7)*1074.0/360.0)/Stamina*50.0)
-		WorkYen = (WorkResult*9800.0/8400.0)
-		return "계산 결과입니다.\n필요한 쥬엘은 " + "%.2f"%WorkResult + "개\nG셋충전시 " + "%.2f"%WorkYen +"엔"
+		Result = score/((170.0/60.0*0.7)+(170.0/60.0*0.7)*1074.0/360.0)/genki*50.0
+		Yen = Result*9800.0/8400.0
 	else:
-		Result = (Score/(170.0/60.0+170.0/60.0*1074.0/360.0)/Stamina*50.0)
+		Result = (score/(170.0/60.0+170.0/60.0*1074.0/360.0)/genki*50.0)
 		Yen = (Result*9800.0/8400.0)
-		return "계산 결과입니다.\n필요한 쥬엘은 " + "%.2f"%Result + "개\nG셋충전시 " + "%.2f"%Yen +"엔"
+	return "계산 결과입니다.\n필요주얼: %.2f\nG셋충전시: %.2f엔" % (Result, Yen)
 	
-def calcTour(Stamina, Score, isWork):
+def calcTour(genki, score, isWork):
 	if isWork:
-		WorkResult = (Score/281.0/Stamina*1500.0)
-		WorkYen = (WorkResult*9800.0/8400.0)
-		return "계산 결과입니다.\n필요한 쥬엘은 " + "%.2f"%WorkResult + "개\nG셋충전시 " + "%.2f"%WorkYen +"엔"
+		Result = score/281.0/genki*1500.0
+		Yen = Result*9800.0/8400.0
 	else:
-		Result = (Score/326.0/Stamina*1500.0)
-		Yen = (Result*9800.0/8400.0)
-		return "계산 결과입니다.\n필요한 쥬엘은 " + "%.2f"%Result + "개\nG셋충전시 " + "%.2f"%Yen +"엔"
+		Result = score/326.0/genki*1500.0
+		Yen = Result*9800.0/8400.0
+	return "계산 결과입니다.\n필요주얼: %.2f\nG셋충전시: %.2f엔" % (Result, Yen)
+
+def calcTune(genki, score, isWork):
+	jaehwa = score * 140 / 537
+	if isWork:
+		genkiUsage = jaehwa * 300 / 525
+	else:
+		genkiUsage = jaehwa * 30 / 75
+
+	jewelUsage = genkiUsage * 50 / genki
+	moneyUsage = jewelUsage * 9800 / 8400
+	return "계산 결과입니다.\n필요주얼: %.2f\nG셋충전시: %.2f엔" % (jewelUsage, moneyUsage)
